d4afb89c6edfe499bde0f8d4ff4334886b61c7c7
Bello Shehu <belloshehu1@gmail.com>
2025-05-18 01:17:49 +0100

    Update index page

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,9 +3,7 @@ import { NextFunction, Request, Response } from "express";
 class IndexController {
 	public index = (req: Request, res: Response, next: NextFunction) => {
 		try {
-			res
-				.status(200)
-				.send("Welcome to EventPro - Event Centers Management API");
+			res.status(200).send("Welcome to DekoStudio API.");
 		} catch (error) {
 			next(error);
 		}
